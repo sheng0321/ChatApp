43be901baf1671947d2d148105cd58198a351e85
sheng <chrisxin911@hotmail.com>
2024-02-22 11:19:27 -0500

    add SignalR client code

---
+++ ChatApp/wwwroot/js/chat.js
@@ -0,0 +1,31 @@
+"use strict";
+
+const { signalR } = require("./signalr/dist/browser/signalr");
+
+var connection = new signalR.HubConnectionBuilder().withUrl("chatHub").build();
+//Disable the send button until connection is established.
+document.getElementById("sendButton").disabled = true;
+
+connection.on("ReceiveMessage", function (user, message) {
+    var li = document.createElement("li");
+    document.getElementById("messageList").append(li);
+
+    li.textContent = `${user} says ${message}`;
+});
+connection.start().then(function () {
+    document.getElementById("sendButton").disabled = false;
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+document.getElementById("sendButton").addEventListener("click", function (event) {
+    var user = document.getElementById("userInput").value;
+    var message = document.getElementById("messageInput").value;
+    connection.invoke("SendMessage", user, message).catch(function (err) {
+        return console.error(err.toString());
+    });
+
+    event.preventDefault();
+    })
+
+)
