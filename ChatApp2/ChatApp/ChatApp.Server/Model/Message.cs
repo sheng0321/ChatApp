762f0e1ba509c58a1b65b6bfff4d7fc9f3ca697b
sheng <chrisxin911@hotmail.com>
2024-02-22 15:21:12 -0500

    chatApp2 is React version

---
+++ ChatApp2/ChatApp/ChatApp.Server/Model/Message.cs
@@ -0,0 +1,11 @@
+﻿namespace ChatApp.Server.Model
+{
+    public class ChatMessage
+    {
+        public int Id { get; set; }
+        public string Message { get; set; }
+        public string User { get; set; }
+
+        public string SendTime { get; set; }= DateTime.Now.ToString();
+    }
+}
---
+++ ChatApp2/ChatApp/ChatApp.Server/Program.cs
@@ -0,0 +1,50 @@
+using ChatApp.Server.Hubs;
+using ChatApp.Server.Model;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddSignalR();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("ClientPermission", policy =>
+    {
+        policy.AllowAnyHeader()
+            .AllowAnyMethod()
+            .WithOrigins("https://localhost:5173")
+            .AllowCredentials();
+    });
+});
+var app = builder.Build();
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseCors("ClientPermission");
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.MapHub<ChatHub>("/hubs/chat");
+
+
+app.MapFallbackToFile("/index.html");
+
+app.Run();
